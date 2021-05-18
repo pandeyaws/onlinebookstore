8e08f61b2d4dd8e48f9b53d43d6493baf954d289
pandeyaws <67418956+pandeyaws@users.noreply.github.com>
2021-05-19 00:19:38 +0530

    IDatabase.java

--- OnlineBookStore/constants/IDatabase.java
+++ OnlineBookStore/constants/IDatabase.java
@@ -3,8 +3,8 @@ package constants;
 public class IDatabase {
 
 	public static final String DRIVER_NAME = "com.mysql.jdbc.Driver";
-	public static final String CONNECTION_STRING = "jdbc:mysql://localhost:3306/onlinebookstore";
-	public static final String USER_NAME = "root";
-	public static final String PASSWORD = "root";
+	public static final String CONNECTION_STRING = "jdbc:mysql://sonarqubedb.ce04qvu0nmld.us-east-1.rds.amazonaws.com:3306/onlinebookstore";
+	public static final String USER_NAME = "sonarqubedb";
+	public static final String PASSWORD = "sonarqubedb";
 
 }
